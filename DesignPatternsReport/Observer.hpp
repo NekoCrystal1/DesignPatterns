ac525954174378c8e7f27f07a390becb2451ffa7
xiao_hei_hand <1919321049@qq.com>
2026-01-07 15:31:37 +0800

    修改基类结构

--- DesignPatternsReport/Observer.hpp
+++ DesignPatternsReport/Observer.hpp
@@ -10,6 +10,7 @@ class Observer
 public:
 	Observer() = default;
 	~Observer() = default;
+	//默认为int状态，当然也可以在对应子类重载主题和观察者相关函数实现其他类型
 	virtual void update(int state) = 0;
 protected:
 	std::string name;
--- DesignPatternsReport/Subject.hpp
+++ DesignPatternsReport/Subject.hpp
@@ -12,40 +12,19 @@ public:
 	Subject() = default;
 	~Subject() = default;
 public:
-	inline void setState(int state);
-	inline int getState() const;
-	inline void attach(Observer* observer);
-	inline void detach(Observer* observer);
-	inline void notify();
+	virtual void attach(Observer* observer);
+	virtual void detach(Observer* observer);
+	virtual void notify() = 0;
 protected:
-	int m_i32state = 0;
 	std::vector<Observer*> m_vecObservers;
 };
-inline void Subject::setState(int state)
-{
-	m_i32state = state;
-	notify();
-}
 
-inline int Subject::getState() const
-{
-	return m_i32state;
-}
-
-inline void Subject::attach(Observer* observer)
+void Subject::attach(Observer* observer)
 {
 	m_vecObservers.push_back(observer);
 }
 
-inline void Subject::detach(Observer* observer)
+void Subject::detach(Observer* observer)
 {
 	m_vecObservers.erase(std::find(m_vecObservers.begin(), m_vecObservers.end(), observer));
-}
-
-inline void Subject::notify()
-{
-	for (Observer* observer : m_vecObservers)
-	{
-		observer->update(m_i32state);
-	}
 }
