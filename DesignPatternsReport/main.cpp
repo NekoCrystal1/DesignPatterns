bffb3b1631545f2e776c23eacb43fa27b62a64c0
xiao_hei_hand <1919321049@qq.com>
2026-01-07 14:23:54 +0800

    初始化并确定设计观察者模式

---
+++ DesignPatternsReport/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @brief 信息系统分析与设计课程报告
+ *
+ * @details 观察者模式的实现
+ *
+ * @author xiao_hei_hand
+ * @email 1919321049@qq.com
+ * @date 2026/1/5
+ *
+ */
+int main()
+{
+	return 0;
+}
