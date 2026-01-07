6d0b60584729f1bac0cdf32ab18ffcb74f234b2a
xiao_hei_hand <1919321049@qq.com>
2026-01-07 16:02:28 +0800

    调整基类

--- DesignPatternsReport/Observer.hpp
+++ DesignPatternsReport/Observer.hpp
@@ -8,10 +8,14 @@
 class Observer
 {
 public:
-	Observer() = default;
+	Observer(std::string name);
 	~Observer() = default;
 	//默认为int状态，当然也可以在对应子类重载主题和观察者相关函数实现其他类型
 	virtual void update(int state) = 0;
 protected:
-	std::string name;
+	std::string m_strName;
 };
+
+Observer::Observer(std::string name) : m_strName(name)
+{
+}
